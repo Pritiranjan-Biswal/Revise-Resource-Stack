cf4c49cbdaf28cde59dcc2eb14f480ebd0e3bfc1
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-09-01 23:33:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Two Consecutive Same/remove-two-consecutive-same.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int removeConsecutiveSame(vector<string>& arr) {
+        // code here
+        stack<string>st;
+        for(int i=0; i<arr.size(); i++) {
+            if(st.empty()) {
+                st.push(arr[i]);
+            }
+            else if(arr[i]==st.top()) {
+                st.pop();
+            }
+            else {
+                st.push(arr[i]);
+            }
+        }
+        return st.size();
+    }
+};
